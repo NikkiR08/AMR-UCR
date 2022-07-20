14b6c1d94f3d26436990e9e494c18fa71597dd0f
NikkiR08 <nichola.r.naylor@gmail.com>
2022-07-20 22:18:18 +0100

    added plotting for paper

--- antibiotic/5_Calculation_of_Outputs.R
+++ antibiotic/5_Calculation_of_Outputs.R
@@ -97,7 +97,7 @@ abx_sc1_sc2[is.na(cost.sc2) & Route.of.Admin.x=="po", cost.sc2 := abx.adj*diff.f
 abx_sc1_sc2[is.na(cost.sc2) & Route.of.Admin.x=="inj", cost.sc2:= abx.adj*diff.fctr]
 
 ## ## save without labels
-save(abx_sc1_sc2, file="Data/antibiotics/sc1_sc2_nolabels.RData")
+save(abx_sc1_sc2, file="antibiotic/outputs/sc1_sc2_nolabels.RData")
 
 ## relabel for outputs
 abx_sc1_sc2_output <- abx_sc1_sc2 %>% rename("Country (ISO3 Code)"="iso3c",
@@ -108,18 +108,29 @@ abx_sc1_sc2_output <- abx_sc1_sc2 %>% rename("Country (ISO3 Code)"="iso3c",
                                       "MSH Dose" = "Dose.x",
                                       "MSH Cost" = "abx.adj",
                                       "Scenario 1 Generic Cost Estimate" = "generic.cost.sc1",
-                                      "Scenario 2 Cost Estimate" = "cost.sc2")  %>%
+                                      "Scenario 2 Income Cost Estimate" = "cost.sc2")  %>%
                           select("Country (ISO3 Code)","WHO-CHOICE Region",
                                  "Income Group" ,  "Antibiotic",
                                  "MSH Form","MSH Dose","MSH Cost",
-                                 "Scenario 1 Generic Cost Estimate","Scenario 2 Cost Estimate" ) %>%
+                                 "Scenario 1 Generic Cost Estimate","Scenario 2 Income Cost Estimate","Class" ,
+                                 "Category") %>%
                           as.data.table()
+### !!! need to add rounding 
 
 write.csv(abx_sc1_sc2_output, "antibiotic/outputs/country_abx_cost.csv")
 save(abx_sc1_sc2_output, file="antibiotic/outputs/country_abx_cost.RData")
 
 ##### REGIONAL AVERAGES ##############
-load("Data/antibiotics/country_abx_cost.RData")
+load("antibiotic/outputs/country_abx_cost.RData")
+load("data_all/who_whoc_wb.RData")
+who <- who_whoc_wb[ , c("iso3c","who.region")]
+
+N <- as.data.table(read.csv("data_all/Population-EstimatesData_092020.csv"))
+N <- N[Indicator.Code=="SP.POP.TOTL"]
+
+### keep 2019 values
+N <- N[ , c("Country.Code","X2019")]
+setnames(N, "X2019", "npop")
 
 combo <- merge(abx_sc1_sc2_output, N, by.x="Country (ISO3 Code)", by.y="Country.Code")
 combo <- merge(combo, who, by.x="Country (ISO3 Code)", by.y="iso3c" )
@@ -128,20 +139,94 @@ combo <- merge(combo, who, by.x="Country (ISO3 Code)", by.y="iso3c" )
 regional.abx <- combo %>%
   filter(!is.na(npop)) %>%
   group_by(who.region, Antibiotic,`MSH Form`, `MSH Dose`) %>% 
-  summarise(weighted_cost_Scenario1 = weighted.mean(`Scenario 1 Generic Cost Estimate`, npop),
-            weighted_cost_Scenario2 = weighted.mean(`Scenario 2 Cost Estimate`, npop)) %>%
+  summarise(weighted_cost_Scenario1 = weighted.mean(`Scenario 1 Generic Cost Estimate`, npop, na.rm=TRUE),
+            weighted_cost_Scenario2 = weighted.mean(`Scenario 2 Cost Estimate`, npop, na.rm=TRUE)) %>%
   as.data.table()
 
+write.csv(regional.abx, file="antibiotic/outputs/regional_abx_cost.csv")
+
+#### BY CLASS AND REGION ######
+
+regional.abx.class <- combo %>%
+  filter(!is.na(npop)) %>%
+  group_by(who.region, Class) %>% 
+  summarise(`Scenario 1 - Generic Proxy` = weighted.mean(`Scenario 1 Generic Cost Estimate`, npop, na.rm=TRUE),
+            `Scenario2 - Income Group Proxy` = weighted.mean(`Scenario 2 Cost Estimate`, npop, na.rm=TRUE)) %>%
+  as.data.table()
+
+rg.abx.cls.melt <- melt(regional.abx.class, id=c("who.region","Class"),
+                        measure=c("Scenario 1 - Generic Proxy",
+                                  "Scenario2 - Income Group Proxy"))
+
+ggplot(data = rg.abx.cls.melt, aes(Class, value, color=variable, shape=variable)) +
+  geom_point(position=position_jitter(h=0.1, w=0.1), alpha = 0.5, size = 2.5) +
+  facet_wrap(~who.region)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  xlab("Antibiotic Class") +
+  ylab("Excess Cost per Unit") +
+  scale_color_brewer(palette="Dark2")
+ 
 #### BY AWARE CATEGORY ######
 
+aware.plot.df <- melt(combo, id=c("Country (ISO3 Code)","Antibiotic","MSH Dose",
+                                  "MSH Form","Class","Category"),
+                           measure=c("Scenario 1 Generic Cost Estimate",
+                                     "Scenario 2 Income Cost Estimate"))
+
+reserve <- combo[Category=="Reserve"]
+unique(reserve$Antibiotic)
+## "linezolid"
+summary(reserve$`Scenario 1 Generic Cost Estimate`)
+summary(reserve$`Scenario 2 Income Cost Estimate`, na.rm=TRUE)
+
+ftable(aware.plot.df$`MSH Dose`)
+## most common are 1000mg, 150mg, 250mg and 500mg
+
+# ### 500 mg oral
+# oral.500 <-aware.plot.df[`MSH Form`=="tab-cap"& 
+#                                          `MSH Dose`=="500mg"]
+# 
+# ggplot(oral.500, aes(x=Category, y=value, fill=variable)) +
+#   geom_boxplot()
+# ### zoom without dropping data
+# ggplot(oral.500, aes(x=Category, y=value, fill=variable)) +
+#   geom_boxplot()+
+#   coord_cartesian(ylim = c(0, 1))
+# 
+# ### 500 mg vial
+# vial.500 <-aware.plot.df[`MSH Form`=="vial"& 
+#                            `MSH Dose`=="500mg"]
+# 
+# ggplot(vial.500, aes(x=Category, y=value, fill=variable)) +
+#   geom_boxplot()
+# ### zoom without dropping data
+# ggplot(vial.5000, aes(x=Category, y=value, fill=variable)) +
+#   geom_boxplot()+
+#   coord_cartesian(ylim = c(0, 3))
+# #### !!! only 2 antibiotic (1 for each group in this category)
+#### for vial so moved to previous large groups
+
+### 250 mg oral
+oral.250 <-aware.plot.df[`MSH Form`=="tab-cap"&
+                           `MSH Dose`=="250mg"]
+unique(oral.250$Antibiotic)
+ggplot(oral.250, aes(x=Category, y=value, fill=variable)) +
+  geom_boxplot()
+### zoom without dropping data
+ggplot(oral.250, aes(x=Category, y=value, fill=variable)) +
+  geom_boxplot()+
+  coord_cartesian(ylim = c(0, 1))
 
 
+### 1000 mg vial
+vial.1000 <-aware.plot.df[`MSH Form`=="vial"& 
+                           `MSH Dose`=="1000mg"]
+unique(vial.1000$Antibiotic)
 
-####**** PLOTS ****#############
-
-
-## regional averages 
-mdata_region <- regional.abx[ , .(`Scenario 1`=median(`Scenario 1`, na.rm=TRUE),
-                                  `Scenario 2`=median(`Scenario 2`, na.rm=TRUE)),
-                              by=.(who.region,`MSH Form`)]
+ggplot(vial.1000, aes(x=Category, y=value, fill=variable)) +
+  geom_boxplot()
+### zoom without dropping data
+ggplot(vial.1000, aes(x=Category, y=value, fill=variable)) +
+  geom_boxplot()+
+  coord_cartesian(ylim = c(0, 3))
 
