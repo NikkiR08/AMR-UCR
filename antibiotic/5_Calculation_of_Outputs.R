b71927fcb510519b19e62544bd95fd0d8c520d1f
NikkiR08 <nichola.r.naylor@gmail.com>
2022-07-20 19:17:21 +0100

    file path and begin regional avs

--- antibiotic/5_Calculation_of_Outputs.R
+++ antibiotic/5_Calculation_of_Outputs.R
@@ -1,10 +1,10 @@
 ######### ANTIBIOTICS COSTING CALCULATIONS ################
-library(tidyverse) 
+library(tidyverse)
 library(data.table)
 library(ggplot2)
 
 ### reading in data
-load("Data/antibiotics/abx_combo_inputs.RData")
+load("antibiotic/outputs/abx_combo_inputs.RData")
 
 ############ SCENARIO 1: GENERIC PRICE ADJUSTMENT FACTORS ########################
 
@@ -12,7 +12,7 @@ load("Data/antibiotics/abx_combo_inputs.RData")
 ## create adjustment factor:
 oral_generic_av <- abx_combo_inputs %>%
   filter(Route.of.Admin.x=="po") %>%
-  group_by(iso3c) %>% 
+  group_by(iso3c) %>%
   summarise(generic_adj_oral = median(generic.adj.x/abx.adj,na.rm=TRUE)) %>%
   as.data.table()
 
@@ -27,7 +27,7 @@ inj_generic_av <- abx_combo_inputs %>%
 median(oral_generic_av$generic_adj_oral)
 median(inj_generic_av$generic_adj_inj)
 
-## merge back in 
+## merge back in
 abx_sc1 <- merge(abx_combo_inputs, oral_generic_av, by="iso3c")
 abx_sc1 <- merge(abx_sc1, inj_generic_av, by="iso3c")
 
@@ -38,7 +38,7 @@ abx_sc1[ , generic.cost.sc1 := generic.adj.x]
 abx_sc1[is.na(generic.cost.sc1), generic.cost.sc1 := generic.adj.y]
 
 ## DESCRIPTIVE STATS
-abx_sc1_des <- abx_sc1[iso3c=="GBR"] ## chose 1 country
+abx_sc1_des <- abx_sc1[iso3c=="CUB"] ## chose 1 country
 length(which(abx_sc1_des$Route.of.Admin.x=="po"))
 length(which(abx_sc1_des$Route.of.Admin.x=="inj"))
 length(unique(abx_sc1$iso3c))
@@ -70,17 +70,17 @@ abx_sc1_sc2[Income.group=="High income" , cost.sc2 := UK.adj.x]
 abx_sc1_sc2[Income.group=="Upper middle income" , cost.sc2 := SA.adj.x]
 abx_sc1_sc2[Income.group=="Lower middle income"|
                    Income.group=="Low income", cost.sc2 := India.adj.x]
-abx_sc1_sc2[Income.group=="High income" & Route.of.Admin.x=="inj", cost.sc2 := UK.adj.y] 
+abx_sc1_sc2[Income.group=="High income" & Route.of.Admin.x=="inj", cost.sc2 := UK.adj.y]
 abx_sc1_sc2[Income.group=="Upper middle income" & Route.of.Admin.x=="inj", cost.sc2 := SA.adj.y]
 abx_sc1_sc2[(Income.group=="Lower middle income"|
               Income.group=="Low income") & Route.of.Admin.x=="inj", cost.sc2 := India.adj.y]
 
 # ## number of combinations with no sc2 cost
-# abx_sc1_sc2$combo <- paste(abx_sc1_sc2$MSH_antibiotic, abx_sc1_sc2$Route.of.Admin.x,
-#                            abx_sc1_sc2$Dose.x)
-# length(unique(abx_sc1_sc2$combo))
-# temp.sc2 <- abx_sc1_sc2[is.na(cost.sc2)]
-# length(unique(temp.sc2$combo))
+abx_sc1_sc2$combo <- paste(abx_sc1_sc2$MSH_antibiotic, abx_sc1_sc2$Route.of.Admin.x,
+                           abx_sc1_sc2$Dose.x)
+length(unique(abx_sc1_sc2$combo))
+temp.sc2 <- abx_sc1_sc2[is.na(cost.sc2)]
+length(unique(temp.sc2$combo))
 
 ## trying with data table (scenario 1 was done with piping)
 df_factor_Y <- abx_sc1_sc2[ , .(diff.fctr=median(cost.sc2/abx.adj, na.rm=TRUE)), by=.(Income.group,
@@ -97,7 +97,7 @@ abx_sc1_sc2[is.na(cost.sc2) & Route.of.Admin.x=="po", cost.sc2 := abx.adj*diff.f
 abx_sc1_sc2[is.na(cost.sc2) & Route.of.Admin.x=="inj", cost.sc2:= abx.adj*diff.fctr]
 
 ## ## save without labels
-## save(abx_sc1_sc2, file="Data/antibiotics/sc1_sc2_nolabels.RData")
+save(abx_sc1_sc2, file="Data/antibiotics/sc1_sc2_nolabels.RData")
 
 ## relabel for outputs
 abx_sc1_sc2_output <- abx_sc1_sc2 %>% rename("Country (ISO3 Code)"="iso3c",
@@ -115,19 +115,29 @@ abx_sc1_sc2_output <- abx_sc1_sc2 %>% rename("Country (ISO3 Code)"="iso3c",
                                  "Scenario 1 Generic Cost Estimate","Scenario 2 Cost Estimate" ) %>%
                           as.data.table()
 
-## write.csv(abx_sc1_sc2_output, "Data/antibiotics/country_abx_cost.csv")
-## save(abx_sc1_sc2_output, file="Data/antibiotics/country_abx_cost.RData")
+write.csv(abx_sc1_sc2_output, "antibiotic/outputs/country_abx_cost.csv")
+save(abx_sc1_sc2_output, file="antibiotic/outputs/country_abx_cost.RData")
+
+##### REGIONAL AVERAGES ##############
+load("Data/antibiotics/country_abx_cost.RData")
+
+combo <- merge(abx_sc1_sc2_output, N, by.x="Country (ISO3 Code)", by.y="Country.Code")
+combo <- merge(combo, who, by.x="Country (ISO3 Code)", by.y="iso3c" )
 
 
-#### read in from regional calculator for descriptive stats ######
-load("Data/antibiotics/regional_abx.RData")
+regional.abx <- combo %>%
+  filter(!is.na(npop)) %>%
+  group_by(who.region, Antibiotic,`MSH Form`, `MSH Dose`) %>% 
+  summarise(weighted_cost_Scenario1 = weighted.mean(`Scenario 1 Generic Cost Estimate`, npop),
+            weighted_cost_Scenario2 = weighted.mean(`Scenario 2 Cost Estimate`, npop)) %>%
+  as.data.table()
 
-## scenario 1 vs scenario 2
-names(regional.abx)[names(regional.abx) == 'weighted_cost_Scenario1'] <- 'Scenario 1'
-names(regional.abx)[names(regional.abx) == 'weighted_cost_Scenario2'] <- 'Scenario 2'
+#### BY AWARE CATEGORY ######
 
-mdata <- melt(regional.abx, id=c("who.region","Antibiotic", "MSH Form", "MSH Dose"))
-ggplot(mdata, aes(x=value, colour=variable)) + geom_density() 
+
+
+
+####**** PLOTS ****#############
 
 
 ## regional averages 
@@ -135,57 +145,3 @@ mdata_region <- regional.abx[ , .(`Scenario 1`=median(`Scenario 1`, na.rm=TRUE),
                                   `Scenario 2`=median(`Scenario 2`, na.rm=TRUE)),
                               by=.(who.region,`MSH Form`)]
 
-mdata_region <- melt(mdata_region, id=c("who.region","MSH Form"))
-mdata_tabcap <- subset(mdata_region, `MSH Form`=="tab-cap")
-mdata_inj <- subset(mdata_region, `MSH Form`=="vial") ## all vial in inj currently due to definitions and mapping
-## might need to adapt the above if in future iterations expand definitions/change mapping assumptions
-
-# regional bar plots:
-# Change the colors manually
-ggplot(data=mdata_tabcap, aes(x=who.region, y=value, fill=`variable`)) +
-  geom_bar(stat="identity", color="black", position=position_dodge())+
-  theme_minimal() + ylab("Cost per unit ($2019)") +
-  ggtitle("Tab-Cap")
-
-ggplot(data=mdata_inj, aes(x=who.region, y=value, fill=`variable`)) +
-  geom_bar(stat="identity", color="black", position=position_dodge())+
-  theme_minimal() + ylab("Cost per unit ($2019)") +
-  ggtitle("Injectable")
-
-### just to show one region - AFRO
-afro <- regional.abx[who.region=="AFRO"]
-
-afrodose <- afro %>%
-  group_by(Antibiotic, `MSH Form`) %>%
-  arrange(`MSH Dose`) %>%
-  slice(n()) %>%
-  ungroup
-
-ggplot(data=afrodose, aes(x=Antibiotic, y=`Scenario 1`, color=`MSH Dose`)) +
-  geom_point() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  ylab("Cost per unit (2019 USD)")
-
-
-## South Africa
-zaf <- abx_sc1_sc2_output[`Country (ISO3 Code)`=="ZAF" & `MSH Form`=="tab-cap"]
-zaf <- zaf %>%
-  group_by(Antibiotic, `MSH Form`) %>%
-  arrange(`MSH Dose`) %>%
-  slice(n()) %>%
-  ungroup %>%
-  arrange(`Scenario 2 Cost Estimate`) %>%
-  select(Antibiotic,"MSH Cost",`Scenario 1 Generic Cost Estimate`,
-         `Scenario 2 Cost Estimate`) %>%
-  as.data.table()
-
-zafG <- melt(zaf, id="Antibiotic")
-
-orderzafG <- zafG[variable=="Scenario 2 Cost Estimate"]
-orderzafG <- orderzafG$Antibiotic[order(orderzafG$value, decreasing=TRUE)]
-
-zafG <- zafG %>%
-    mutate(Antibiotic=factor(Antibiotic, levels=orderzafG))
-
-ggplot(data=zafG, aes(x=Antibiotic, y=value, fill=`variable`)) +
-  geom_bar(stat="identity", color="black", position=position_dodge())+
-  coord_flip() + xlab("Cost per Unit (2019 USD)")
