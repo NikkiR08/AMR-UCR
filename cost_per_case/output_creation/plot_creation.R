7625d41e384525e3c124a1c878c47e6e90d304e2
Nichola Naylor <nichola.naylor@phe.gov.uk>
2023-05-30 22:28:28 +0100

    starting map plots for unit costs

--- cost_per_case/output_creation/plot_creation.R
+++ cost_per_case/output_creation/plot_creation.R
@@ -4,6 +4,7 @@ library(data.table)
 library(ggplot2)
 library(tidyr)
 library(gcookbook) ## for colour blind friendly palette
+library(maptools)
 library(rworldmap)
 library(dplyr)
 
@@ -26,7 +27,129 @@ mine.heatmap <- ggplot(data = evidence_heatmap, mapping = aes(x = Syndrome,
 mine.heatmap
 
 
-######## REGIONAL PLOTS - FIGURE 3 ######
+
+
+##### PLOTS OF DIFFERENT TYPES OF COST - supplementary material ######
+
+load("cost_per_case/outputs/costing.table.region.G.RData")
+
+outputs <- as.data.table(costing.table.region.G)
+outputs[LOW_weighted_costing.both<=0 &
+          HIGH_weighted_costing.both<=0 , cost.sig.flag:="SIG"]
+outputs[LOW_weighted_costing.both>=0 &
+          HIGH_weighted_costing.both>=0 , cost.sig.flag:="SIG"]
+outputs[LOW_weighted_costing.both<0 &
+          HIGH_weighted_costing.both>0 , cost.sig.flag := "NONSIG" ]
+
+outputs.cost.sig <- outputs[cost.sig.flag=="SIG"]
+
+AMR <- outputs.cost.sig[AMR_or_DRI=="AMR"]
+
+colnames(AMR)[colnames(AMR) == 'AV_weighted_costing.both'] <- 'Scenario 1. Combined Sample'
+colnames(AMR)[colnames(AMR) == 'AV_weighted_costing.sc2'] <- 'Scenario 2. Applied Adjustment Factor'
+colnames(AMR)[colnames(AMR) == 'AV_weighted_costing.los'] <- 'LOS Cost Only'
+
+
+AMR.melt <- melt(AMR, id=c("who.region", "syndrome", "class", "AMR_or_DRI", "gram.stain"),
+                 measure=c("Scenario 1. Combined Sample","Scenario 2. Applied Adjustment Factor","LOS Cost Only"))
+
+AMR.melt <- AMR.melt[class!="mdr"] ## remove TB to discuss separately
+
+ggplot(data = AMR.melt, aes(class, value, color=variable, shape=variable)) +
+  geom_point(position=position_jitter(h=0.1, w=0.1), alpha = 0.5, size = 2.5) +
+  facet_grid(who.region ~ syndrome)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  xlab("Resistance Exposure") +
+  ylab("Excess Hospital Cost per Case (2019 USD)") +
+  scale_color_brewer(palette="Dark2")
+
+DRI <- outputs.cost.sig[AMR_or_DRI=="DRI"]
+
+colnames(DRI)[colnames(DRI) == 'AV_weighted_costing.both'] <- 'Scenario 1. Combined Sample'
+colnames(DRI)[colnames(DRI) == 'AV_weighted_costing.sc2'] <- 'Scenario 2. Applied Adjustment Factor'
+colnames(DRI)[colnames(DRI) == 'AV_weighted_costing.los'] <- 'LOS Cost Only'
+
+
+DRI.melt <- melt(DRI, id=c("who.region", "syndrome", "class", "AMR_or_DRI", "gram.stain"),
+                 measure=c("Scenario 1. Combined Sample","Scenario 2. Applied Adjustment Factor","LOS Cost Only"))
+
+DRI.melt <- DRI.melt[class!="mdr"] ## remove TB to discuss separately
+
+ggplot(data = DRI.melt, aes(class, value, color=variable, shape=variable)) +
+  geom_point(position=position_jitter(h=0.1, w=0.1), alpha = 0.5, size = 2.5) +
+  facet_grid(who.region ~ syndrome)+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  xlab("Resistance Exposure") +
+  ylab("Excess Hospital Cost per Case (2019 USD)") +
+  scale_color_brewer(palette="Dark2")+
+  ggtitle("DRI")
+
+#### MAP PLOTS #######
+###### PLOTS ################
+
+#### add code to log numerical columns for plots
+load("cost_per_case/outputs/scenario2.results.RData")
+
+sc2.results.sum <-  sc2.results.long[, lapply(.SD, mean, na.rm=TRUE),
+            by = c("Country (ISO3 Code)",
+                   "AMR_or_DRI"),
+            .SDcols=c("Mean Cost - Across Both")]
+
+
+joinData <- joinCountryData2Map( sc2.results.sum ,
+                                 joinCode = "ISO3",
+                                 nameJoinColumn = "Country (ISO3 Code)")
+
+mapping_function <- function(x, y){
+  ##!!!
+  ## doing alot by hand post production for now but next time try to integrate labels
+  ## e.g. adding back transformed scale numbers and titles
+  theMap <- mapCountryData( x, nameColumnToPlot=y, addLegend=FALSE ,
+                            colourPalette = "terrain", mapTitle = y)
+  
+  labs <- theMap$cutVector %>% 
+    exp() %>%
+    round(-2)
+  
+  do.call( addMapLegend, c(theMap,
+                           legendWidth=1, legendMar = 2,
+                           legendIntervals='data',
+                           legendLabels='all'))
+  
+  print(labs)
+}
+
+mapping_function(joinData, "Mean Cost - Across Both")
+mapping_function(joinData, "sc2_ecoli_TAD_lg")
+
+## remove log for % diff maps
+mapping_function_nolog <- function(x, y){
+  ##!!!
+  ## doing alot by hand for now but next time try to integrate labels
+  ## e.g. adding back transformed scale numbers and titles
+  theMap <- mapCountryData( x, nameColumnToPlot=y, addLegend=FALSE ,
+                            colourPalette = "terrain", mapTitle = y)
+  
+  labs <- theMap$cutVector
+  labs <- format(labs, scientific=FALSE)
+  
+  do.call( addMapLegend, c(theMap,
+                           legendWidth=1, legendMar = 2,
+                           legendIntervals='data',
+                           legendLabels='all'))
+  
+  print(labs)
+}
+
+joinData <- joinCountryData2Map( results_GDP,
+                                 joinCode = "ISO3",
+                                 nameJoinColumn = "iso3c")
+mapping_function_nolog(joinData, "sc1_ecoli_TPD")
+mapping_function_nolog(joinData, "sc2_ecoli_TPD")
+
+
+####******************* PLots not in use currently******************** ####
+######## REGIONAL PLOTS for Hospital Costs######
 load("cost_per_case/outputs/costing.table.region.G.RData")
 
 outputs <- as.data.table(costing.table.region.G)
@@ -38,20 +161,20 @@ AMR_GNGP <- AMR[class!="mdr"] ## put TB on separate plots
 
 LOS.plots <- function(EURO){
   name.reg <- EURO$who.region[1]
-x <- ggplot(EURO, aes(x=interaction(class), y=AV_weighted_costing.los, fill=syndrome)) + 
-  geom_bar(position=position_dodge(preserve="single"), stat="identity",
-           colour="black", # Use black outlines,
-           size=.3) +      # Thinner lines
-  geom_errorbar(aes(ymin=LOW_weighted_costing.los, ymax=HIGH_weighted_costing.los),
-                size=.3,    # Thinner lines
-                width=.2,
-                position = position_dodge(width = 0.9, preserve = "single")) +
-  xlab("Resistance Exposure") +
-  ylab("LOS-associated Cost (2019 USD)") +
-  scale_fill_viridis_d()+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  coord_cartesian(ylim=c(-5000, 35000), expand = FALSE)+
-  ggtitle(name.reg)
+  x <- ggplot(EURO, aes(x=interaction(class), y=AV_weighted_costing.los, fill=syndrome)) + 
+    geom_bar(position=position_dodge(preserve="single"), stat="identity",
+             colour="black", # Use black outlines,
+             size=.3) +      # Thinner lines
+    geom_errorbar(aes(ymin=LOW_weighted_costing.los, ymax=HIGH_weighted_costing.los),
+                  size=.3,    # Thinner lines
+                  width=.2,
+                  position = position_dodge(width = 0.9, preserve = "single")) +
+    xlab("Resistance Exposure") +
+    ylab("LOS-associated Cost (2019 USD)") +
+    scale_fill_viridis_d()+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+    coord_cartesian(ylim=c(-5000, 35000), expand = FALSE)+
+    ggtitle(name.reg)
   return(x)
 }
 
@@ -89,62 +212,7 @@ LOS.plots(SEARO)
 LOS.plots(WPRO)
 
 
-##### PLOTS OF DIFFERENT TYPES OF COST - FIGURE 4 ######
-
-load("cost_per_case/outputs/costing.table.region.G.RData")
-
-outputs <- as.data.table(costing.table.region.G)
-outputs[LOW_weighted_costing.both<=0 &
-          HIGH_weighted_costing.both<=0 , cost.sig.flag:="SIG"]
-outputs[LOW_weighted_costing.both>=0 &
-          HIGH_weighted_costing.both>=0 , cost.sig.flag:="SIG"]
-outputs[LOW_weighted_costing.both<0 &
-          HIGH_weighted_costing.both>0 , cost.sig.flag := "NONSIG" ]
-
-outputs.cost.sig <- outputs[cost.sig.flag=="SIG"]
-
-AMR <- outputs.cost.sig[AMR_or_DRI=="AMR"]
-
-colnames(AMR)[colnames(AMR) == 'AV_weighted_costing.both'] <- 'Scenario 1. Combined Sample'
-colnames(AMR)[colnames(AMR) == 'AV_weighted_costing.sc2'] <- 'Scenario 2. Applied Adjustment Factor'
-colnames(AMR)[colnames(AMR) == 'AV_weighted_costing.los'] <- 'LOS Cost Only'
-
-
-AMR.melt <- melt(AMR, id=c("who.region", "syndrome", "class", "AMR_or_DRI", "gram.stain"),
-                 measure=c("Scenario 1. Combined Sample","Scenario 2. Applied Adjustment Factor","LOS Cost Only"))
-  
-AMR.melt <- AMR.melt[class!="mdr"] ## remove TB to discuss separately
-
-ggplot(data = AMR.melt, aes(class, value, color=variable, shape=variable)) +
-  geom_point(position=position_jitter(h=0.1, w=0.1), alpha = 0.5, size = 2.5) +
-  facet_grid(who.region ~ syndrome)+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  xlab("Resistance Exposure") +
-  ylab("Excess Hospital Cost per Case (2019 USD)") +
-  scale_color_brewer(palette="Dark2")
-
-DRI <- outputs.cost.sig[AMR_or_DRI=="DRI"]
-
-colnames(DRI)[colnames(DRI) == 'AV_weighted_costing.both'] <- 'Scenario 1. Combined Sample'
-colnames(DRI)[colnames(DRI) == 'AV_weighted_costing.sc2'] <- 'Scenario 2. Applied Adjustment Factor'
-colnames(DRI)[colnames(DRI) == 'AV_weighted_costing.los'] <- 'LOS Cost Only'
-
-
-DRI.melt <- melt(DRI, id=c("who.region", "syndrome", "class", "AMR_or_DRI", "gram.stain"),
-                 measure=c("Scenario 1. Combined Sample","Scenario 2. Applied Adjustment Factor","LOS Cost Only"))
-
-DRI.melt <- DRI.melt[class!="mdr"] ## remove TB to discuss separately
-
-ggplot(data = DRI.melt, aes(class, value, color=variable, shape=variable)) +
-  geom_point(position=position_jitter(h=0.1, w=0.1), alpha = 0.5, size = 2.5) +
-  facet_grid(who.region ~ syndrome)+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  xlab("Resistance Exposure") +
-  ylab("Excess Hospital Cost per Case (2019 USD)") +
-  scale_color_brewer(palette="Dark2")+
-  ggtitle("DRI")
-
-##### TB Figure 5 ##########
+##### TB cost per case ##########
 load("cost_per_case/outputs/costing.table.region.G.RData")
 
 outputs <- as.data.table(costing.table.region.G)
@@ -159,7 +227,7 @@ ggplot(tb, aes(x=who.region, y=AV_weighted_costing.los)) +
                 size=.3,    # Thinner lines
                 width=.2,
                 position = position_dodge(width = 0.9, preserve = "single")) 
- 
+
 ggplot(tb, aes(x=who.region, y=AV_weighted_costing.both)) + 
   geom_bar(position=position_dodge(preserve="single"), stat="identity",
            colour="black", # Use black outlines,
